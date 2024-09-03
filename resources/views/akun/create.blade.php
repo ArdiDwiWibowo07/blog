d4242f1777b620d3ae036ccd3eddfa4dfa8711e0
ardi_mimin <ardi.wibowo@mimin.io>
2024-09-03 12:15:42 +0700

    [updated]

--- resources/views/akun/create.blade.php
+++ resources/views/akun/create.blade.php
@@ -8,6 +8,25 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body style="background: lightgray">
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <a class="navbar-brand" href="#">Blog</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('account') }}">Account</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('post') }}">Post</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
     <div class="container mt-5 mb-5">
         <div class="row">
--- resources/views/akun/index.blade.php
+++ resources/views/akun/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +9,27 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 </head>
+
 <body style="background: lightgray">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <a class="navbar-brand" href="#">Blog</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('account') }}">Account</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('post') }}">Post</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
     <div class="container mt-5">
         <div class="row">
@@ -18,15 +39,15 @@
                         <a href="{{ route('account.create') }}" class="btn btn-md btn-success mb-3">Tambah Akun</a>
                         <table class="table table-bordered">
                             <thead>
-                              <tr>
-                                <th scope="col">Username</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Role</th>
-                                <th scope="col">Aksi</th>
-                              </tr>
+                                <tr>
+                                    <th scope="col">Username</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Role</th>
+                                    <th scope="col">Aksi</th>
+                                </tr>
                             </thead>
                             <tbody>
-                              @forelse ($accounts as $account)
+                                @forelse ($accounts as $account)
                                 <tr>
                                     <td>{{ $account->username }}</td>
                                     <td>{{ $account->name }}</td>
@@ -41,36 +62,37 @@
                                         </form>
                                     </td>
                                 </tr>
-                              @empty
-                                  <div class="alert alert-danger">
-                                      Data Akun belum Tersedia.
-                                  </div>
-                              @endforelse
+                                @empty
+                                <div class="alert alert-danger">
+                                    Data Akun belum Tersedia.
+                                </div>
+                                @endforelse
                             </tbody>
-                          </table>  
-                          {{ $accounts->links() }}
+                        </table>
+                        {{ $accounts->links() }}
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
     <script>
         //message with toastr
-        @if(session()->has('success'))
-        
-            toastr.success('{{ session('success') }}', 'BERHASIL!'); 
+        @if(session() -> has('success'))
 
-        @elseif(session()->has('error'))
+        toastr.success('{{ session('success') }}', 'BERHASIL!');
 
-            toastr.error('{{ session('error') }}', 'GAGAL!'); 
-            
+        @elseif(session() -> has('error'))
+
+        toastr.error('{{ session('error') }}', 'GAGAL!');
+
         @endif
     </script>
 
 </body>
+
 </html>
