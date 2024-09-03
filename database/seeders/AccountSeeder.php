338147c699dd7fbab29e34fe317ab1852519ed1f
ardi_mimin <ardi.wibowo@mimin.io>
2024-09-03 13:27:19 +0700

    [updated]

---
+++ database/seeders/AccountSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Account;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AccountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Account::create([
+            'username' => 'admin',
+            'name' => 'admin',
+            'role' => 'admin',
+            'password' => bcrypt('123456'),
+        ]);
+    }
+}
